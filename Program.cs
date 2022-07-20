fff086b1c0cf839298e46f40cfd54fc3510504d5
wakhsavior <wakhsavior@gmail.com>
2022-07-20 22:46:16 +0300

    Составляется комбинация из индексов, вычисление количества вариантов неэффективно, да и не нужно

--- Program.cs
+++ Program.cs
@@ -42,8 +42,10 @@ void EnumerateAllValue(int[] array, int i, int n)
     }
 }
 
-void Perestanovka(int[] array, int i, int n)
+void Perestanovka(int[] array, int i, int n, ulong summ)
 {
+
+    int[][] variants = new int[summ][];
     int j = 0;
     if (i > 0)
     {
@@ -56,23 +58,27 @@ void Perestanovka(int[] array, int i, int n)
 
         if (i + 1 < length)
         {
-            Perestanovka(array, i + 1, n);
+            Perestanovka(array, i + 1, n, summ);
         }
         else
         {
-
+            // variants[arrayIndex] = new int[i];
+            // variants[arrayIndex] = array;
+            // Console.Write($"{arrayIndex} - ");
             PrintArray(array);
+            // arrayIndex++;
             Thread.Sleep(200);
         }
         j++;
     }
+  
 }
 
 // Console.WriteLine("Enter number N:");
 // int n = Convert.ToInt32(Console.ReadLine());
 
 
-int[] cables = { 38, 40, 40, 26, 26, 24, 24, 22, 22, 21, 21, 19,35,65,32,65,43 };
+int[] cables = { 38, 40, 40, 26, 26, 24, 24, 22, 22, 21, 21, 19, 35, 65, 32, 65, 43 };
 
 int n = cables.Length;
 
@@ -85,11 +91,14 @@ for (int i = 1; i <= n; i++)
 }
 Console.WriteLine($"Количество вариантов для {n} кабелей = {summOptions}");
 
-// for (int i = 1; i <= n; i++)
-// {
-//     int[] array = new int[i];
-//     Perestanovka(array, 0, n);
+int[][] variants = new int[summOptions][];
 
-// }
+int arrayIndex = 0;
+for (int i = 1; i <= n; i++)
+{
+    int[] array = new int[i];
+    Perestanovka(array, 0, n, summOptions);
+
+}
 
 
